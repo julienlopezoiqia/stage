6a400f6bb3c84e01b6f6cd2e2d2cbcdfe86d36dc
Julien Lopez <julien.lopez@oiqia.com>
2020-07-28 10:46:16 +0200

    ajout répertoire config

---
+++ config/db.config.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'host' => 'localhost',
+    'username' => 'root',
+	'pswrd'=> 'mo2passe',
+	'db'=>'oiqia_website'
+);
+?>
--- db.config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'host' => 'localhost',
-    'username' => 'root',
-	'pswrd'=> 'LILY081196@t',
-	'db'=>'test'
-);
-?>
