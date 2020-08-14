b8d79094fc4fa42a1b7788c0ebe2fba5eaa6fe57
Thilleli <64068415+Thilleli@users.noreply.github.com>
2020-08-14 09:00:04 +0200

    Add files via upload
    Ajout de la barre "active".

--- Component/navbar.php
+++ Component/navbar.php
@@ -1,20 +1,25 @@
- 
+<?php
+	// On recupere l'URL de la page pour ensuite affecter class = "active" aux liens de nav
+	$page = $_SERVER['REQUEST_URI'];
+	  
+
+?>
 
 
 
-	<div>
-		<nav class="navbar navbar-expand-sm navbar-dark bg-dark ">
-			
-			<ul class="navbar-nav" id="nav" >
-				
+<div>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark ">
 
-					<li class="nav-item" ><a class="nav-link" href="/index.php">Contact</a></li>
-					<li class="nav-item"><a class="nav-link" href="/interested.php">Interested</a></li>
-					<li class="nav-item"><a class="nav-link" href="/newsletter.php">Newsletter</a></li>
-					<li class="nav-item"><a class="nav-link" href="/formulaire.php">Formulaire</a></li>
-					
-				
-			</ul>
-			 
-		</nav>
-	</div>
+<ul class="navbar-nav" id="nav" >
+
+
+<li <?php if(strpos($page, "index.php") !== false){echo 'class="active"';} ?>><a class="nav-link" href="index.php">Contact</a></li>
+<li  <?php if(strpos($page, "interested.php") !== false){echo 'class="active"';} ?>><a class="nav-link" href="interested.php">Interested</a></li>
+<li <?php if(strpos($page, "newsletter.php") !== false){echo 'class="active"';} ?>><a class="nav-link" href="newsletter.php">Newsletter</a></li>
+<li  <?php if(strpos($page, "formulaire.php") !== false){echo 'class="active"';} ?>><a class="nav-link" href="formulaire.php">Formulaire</a></li>
+
+
+</ul>
+
+</nav>
+</div>
