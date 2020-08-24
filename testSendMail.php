5df28010105e6f226cc187015f53d150ac764da6
Thilleli <64068415+Thilleli@users.noreply.github.com>
2020-08-24 08:43:14 +0200

    Add files via upload
    -Rajout du paramètre"nb_jours" à la fonction qui récupère les emails de la bdd. 
    -Rajout de deux boucles for, une parcours la liste des emails et l'autre parcours le tableau de dates.

--- testSendMail.php
+++ testSendMail.php
@@ -1,45 +1,49 @@
-
 <?php
 include('Serveur/DBManager.php');
+$jours= include('Serveur/jours.config.php');
+
+//----------------------------------------------------------------------------------------
 	
-	
-	
-function get_Email_List(){
+function get_Email_List($nb_jours){
 		
-		$request= "SELECT * FROM newsletter n LEFT JOIN formulaire f ON n.email= f.email WHERE DATEDIFF(CURDATE(), n.created_at)>2 AND f.email IS NULL ORDER BY n.created_at DESC";
+		$request= "SELECT n.email FROM newsletter n LEFT JOIN formulaire f ON n.email= f.email WHERE DATEDIFF(CURDATE(), n.created_at) = ".$nb_jours." AND f.email IS NULL ";
 		$result= getResults($request);
-		while ($row = $result -> fetch_array( MYSQLI_NUM)) {
-						printf('<tr bgcolor="#C0C0C0"><td>' .$row[1].'</td><td>'. $row[2].'</td> <br>');
-							echo '</tr>';
-					}
-
-			return $result;
-	}
-$EList = get_Email_List();
-return $EList;
-
-
-
-
-function Send_Email($EList){
-		
-		ini_set('display_errors',1);
-		error_reporting(E_ALL);
-		$from="lilybelhocine006@gmail.com";
-		$subject="Checking PHP mail";
-		$message="sdfghjké";
-		$headers= "From:".$from;
-	
-		if(mail($EList, $subject, $message, $headers)){
-			echo("Email successfully sent");
+		$emails=array();
+		while ($row = $result -> fetch_array( MYSQLI_NUM)){
+			
+			$emails[]=$row[0];
 		}
-		else{
-			echo ("erreur d'envoie");
-		}
-   
+		return $emails;
 }
-$SEmail=Send_Email_List();
-return $SEmail;
 
+//----------------------------------------------------------------------------------------
+
+function Send_Email($Email){
 	
+	
+	$sujet = "Email de test";
+	$corp = "Salut ceci est un email de test envoyer par un script PHP";
+	$headers = "From: email_de_l'entreprise@oiqia.com";
+	if (mail($Email, $sujet, $corp, $headers)) {
+		echo "Email envoyé avec succès  ...";
+	} else {
+		echo "echec...";
+	}
+}
+/*$get=get_Email_List(4);
+print_r(array_values($get));*/
+
+
+//----------------------------------------------------------------------------------------
+
+for ($i=0;$i<sizeof($jours);$i++){
+	$nb_jours = $jours[$i];
+	$emails=get_Email_List($nb_jours);
+	for ($j=0;$j<sizeof($emails);$j++){
+		Send_Email($emails[$j]);
+	}
+	
+}
+
+  
 ?>
